28bd9e4b82634aa5f34e21348f52b0fdd5f7f446
Michael Schurter <schmichael@hashicorp.com>
2018-02-23 16:45:57 -0800

    filterByTainted node should always migrate non-terminal migrating allocs

--- scheduler/reconcile_util.go
+++ scheduler/reconcile_util.go
@@ -199,33 +199,46 @@ func (a allocSet) filterByTainted(nodes map[string]*structs.Node) (untainted, mi
 	migrate = make(map[string]*structs.Allocation)
 	lost = make(map[string]*structs.Allocation)
 	for _, alloc := range a {
-		n, ok := nodes[alloc.NodeID]
-		if !ok {
+		// Terminal allocs are always untainted as they should never be migrated
+		if alloc.TerminalStatus() {
 			untainted[alloc.ID] = alloc
 			continue
 		}
+
+		// Non-terminal allocs that should migrate should always migrate
+		if alloc.DesiredTransition.ShouldMigrate() {
+			migrate[alloc.ID] = alloc
+			continue
+		}
+
+		//FIXME is this neccessary?! Surely RanSuccessfully() is only
+		//      true if the alloc is Terminal?
 
 		// If the job is batch and finished successfully, the fact that the
 		// node is tainted does not mean it should be migrated or marked as
 		// lost as the work was already successfully finished. However for
 		// service/system jobs, tasks should never complete. The check of
 		// batch type, defends against client bugs.
-		if alloc.Job.Type == structs.JobTypeBatch && alloc.RanSuccessfully() {
+		if alloc.Job != nil && alloc.Job.Type == structs.JobTypeBatch && alloc.RanSuccessfully() {
 			untainted[alloc.ID] = alloc
 			continue
 		}
 
-		if !alloc.TerminalStatus() {
-			if n == nil || n.TerminalStatus() {
-				lost[alloc.ID] = alloc
-			} else if alloc.DesiredTransition.ShouldMigrate() {
-				migrate[alloc.ID] = alloc
-			} else {
-				untainted[alloc.ID] = alloc
-			}
-		} else {
+		n, ok := nodes[alloc.NodeID]
+		if !ok {
+			//FIXME why are unknown nodes untainted?! shouldn't this be "lost"?!
 			untainted[alloc.ID] = alloc
+			continue
 		}
+
+		// allocs on gc'd (nil) or lost nodes are Lost
+		if n == nil || n.TerminalStatus() {
+			lost[alloc.ID] = alloc
+			continue
+		}
+
+		// all other allocs are untainted
+		untainted[alloc.ID] = alloc
 	}
 	return
 }
