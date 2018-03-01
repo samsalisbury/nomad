d554a39c7009ce3d67d6841c3c89fa4756bafa2e
Sam Salisbury <samsalisbury@gmail.com>
2018-03-01 14:09:58 +0000

    much more efficient StartStopper impl
    - No longer a busy loop.

--- nomad/startstopper.go
+++ nomad/startstopper.go
@@ -3,61 +3,41 @@ package nomad
 import "sync"
 
 type StartStopper struct {
-	stopped   bool
-	stoppedCh chan struct{}
-	runningCh chan struct{}
+	stoppedCh *chan struct{}
 	sync.RWMutex
 }
 
 func NewStartStopper() *StartStopper {
-	s := &StartStopper{}
-	go func() {
-		for {
-			s.Lock()
-			if s.stopped {
-				s.Unlock()
-				select {
-				default:
-				case s.stoppedCh <- struct{}{}:
-				}
-			} else {
-				s.Unlock()
-				select {
-				default:
-					s.runningCh <- struct{}{}
-				}
-			}
-		}
-	}()
-	return s
+	ch := make(chan struct{})
+	return &StartStopper{stoppedCh: &ch}
 }
 
 func (s *StartStopper) Stop() {
 	s.Lock()
 	defer s.Unlock()
-	s.stopped = true
+	close(*s.stoppedCh)
 }
 
 func (s *StartStopper) Start() {
 	s.Lock()
 	defer s.Unlock()
-	s.stopped = false
+	ch := make(chan struct{})
+	s.stoppedCh = &ch
 }
 
 func (s *StartStopper) Stopped() <-chan struct{} {
-	return s.stoppedCh
+	s.RLock()
+	defer s.RUnlock()
+	return *s.stoppedCh
 }
 
 func (s *StartStopper) IsStopped() bool {
 	s.RLock()
 	defer s.RUnlock()
-	return s.stopped
-}
-
-func (s *StartStopper) Started() <-chan struct{} {
-	return s.runningCh
-}
-
-func (s *StartStopper) WaitForStart() {
-	<-s.runningCh
+	select {
+	default:
+		return false
+	case <-*s.stoppedCh:
+		return true
+	}
 }
